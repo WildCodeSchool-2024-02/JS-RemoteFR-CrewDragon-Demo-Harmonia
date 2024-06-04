e226a49e29fa4d02596f2abf70c62df820ee7084
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-06-04 16:19:08 +0200

    ✨ (index.js): Add express framework and set up a basic server with a route to respond with "plop" on root endpoint 🔧 (package.json): Update "dev" script to use nodemon for automatic server restarts during development ⬆️ (package.json): Add express as a dependency and nodemon as a devDependency to manage server and development environment efficiently

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("coucou c'est nous, c'est derrick");
+const express = require("express");
+const PORT = 3310;
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("plop");
+});
+
+app.listen(PORT, () => {
+	console.log("L'application tourne sur le port : ", PORT);
+});
